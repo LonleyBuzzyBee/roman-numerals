762aa859f241ff17c7f03b28fda97c82e08cd319
mai cook <mailiecook@yahoo.com>
2020-04-02 13:11:30 -0700

    debuging scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,36 @@
-// // business logic
 
-function myFunc(number) {
-
+function myFunc(number) {   //1-10
+  
   var letter = number.split("");
   console.log(letter);
   var roman = ["I","V","X"]; //array
-
+  
 
   if(number <= 3){
     letter = roman[0].repeat(number);
     $(".result").text(letter);
   }else if (number == 4 ){
-    letter = roman[0].repeat(number-[3]);
+    letter = roman[0].repeat(number-[3]); 
     $(".result").text(letter+roman[1]);
   } else if (number == 5) {
     letter = roman[1];
     $(".result").text(letter);
+    //1-5
   } else if (number >= 5 && number < 9) {
     letter = roman[1] + roman[0].repeat(number-[5]);
     $(".result").text(letter);
   } else if (number == 9 ){
-    letter = roman[2];
-    $(".result").text(letter+roman[0]);
-  
+    letter = roman[0].repeat(number-[8]);
+    $(".result").text(letter+roman[2]);
+    // letter = roman[2];
+    // $(".result").text(roman[0]+letter);
   }
 }
-
+ function myFuncTwo (number){
+   var numOverTen = ""
+   
+    $(".result").text(roman[2]+letter);
+ }
 
 // 1   "I"  -add
 // 2   "II" -add
@@ -54,3 +59,60 @@ $(document).ready(function(){
       // $(".result").text(myFunc(numbersInput));
   });
 });
+
+// // // business logic
+
+// function myFunc(number) {
+
+//   var letter = number.split("");
+//   console.log(letter);
+//   var roman = ["I","V","X"]; //array
+
+
+//   if(number <= 3){
+//     letter = roman[0].repeat(number);
+//     $(".result").text(letter);
+//   }else if (number == 4 ){
+//     letter = roman[0].repeat(number-[3]);
+//     $(".result").text(letter+roman[1]);
+//   } else if (number == 5) {
+//     letter = roman[1];
+//     $(".result").text(letter);
+//   } else if (number >= 5 && number < 9) {
+//     letter = roman[1] + roman[0].repeat(number-[5]);
+//     $(".result").text(letter);
+//   } else if (number == 9 ){
+//     letter = roman[2];
+//     $(".result").text(letter+roman[0]);
+  
+//   }
+// }
+
+
+// // 1   "I"  -add
+// // 2   "II" -add
+// // 3   "III"-add
+// // 4   "IV" -subtract from new symbol
+// // 5   "V"  -a new symbol
+
+// // 6   "VI" -add
+// // 7   "VII" -add
+// // 8   "VIII" -add
+// // 9   "IX" -subtract from new symbol
+// // 10  "X"  -a new symbol
+
+
+
+
+
+
+
+// // user interface logic
+// $(document).ready(function(){
+//   $("form#form-container").submit(function(event){
+//     event.preventDefault();
+//     var numbersInput =$("input#user-input").val();
+//       myFunc(numbersInput);
+//       // $(".result").text(myFunc(numbersInput));
+//   });
+// });
